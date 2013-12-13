65127f6c4a09a428f8091fcf1770601db7acc53a
Nikki Thean <nikki.thean@gmail.com>
2013-12-13 07:14:33 -0500

    Delete old files, add styles

--- app.rb
+++ app.rb
@@ -1,14 +1,53 @@
 require 'bundler'
 Bundler.require
 
-Dir('./lib').glob('.rb') do |file|
+Dir.glob('./lib/*.rb') do |file|
   require file
 end
 
 module Quiz
   class App < Sinatra::Application
+    before do
+      @questions = get_questions
+      @options = get_options
+    end
+
     get '/' do
-      
+      erb :quiz
+    end
+
+    post '/result' do
+      result = params.values.map {|value| value.to_i}.reduce(:+)
+      if result < 0
+        @message = "You are Ashley!
+        You are fond of doges and 
+        You are awesome."
+        @image = "ashley_image"
+      elsif result > 0
+        @message = "You are Blake!
+        You are awesome."
+        @image = "blake_image"
+      else
+        @message = "A WILD BLASHLEY APPEARS!
+
+        You are a rare blend of "
+        @image = "blashley_image"
+      end
+      erb :result
+    end
+
+    helpers do
+      def get_questions
+        all = File.read("./lib/questions.txt").split("\n\n")
+        all.map {|question| question.gsub(/\- [\w\W]*/, "")}
+      end
+
+      def get_options
+        all = File.read("./lib/questions.txt").split("\n\n")
+        all.map do |section|
+          section.scan(/\- (.*)/).flatten
+        end
+      end
     end
   end
 end
